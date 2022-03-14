bb8d4f74a3316c0bbc167ff99f07d61ff595da97
anxigy <gy010806@gmail.com>
2022-03-14 17:41:12 +0900

    project init

---
+++ src/Components/Slider.tsx
@@ -0,0 +1,158 @@
+import { AnimatePresence, motion } from "framer-motion";
+import React, { useState } from "react";
+import { useQuery } from "react-query";
+import { useHistory } from "react-router-dom";
+import styled from "styled-components";
+import { IGetMoviesResult } from "../api";
+import { makeImagePath } from "../utils";
+
+const SliderWrapper = styled.div`
+  position: relative;
+  top: -100px;
+`;
+
+const Row = styled(motion.div)`
+  display: grid;
+  gap: 5px;
+  grid-template-columns: repeat(6, 1fr);
+  position: absolute;
+  width: 100%;
+`;
+
+const Box = styled(motion.div)<{ bgPhoto: string }>`
+  background-color: white;
+  background-image: url(${(props) => props.bgPhoto});
+  background-size: cover;
+  background-position: center;
+  height: 180px;
+  font-size: 66px;
+  cursor: pointer;
+  &:first-child {
+    transform-origin: center left;
+  }
+  &:last-child {
+    transform-origin: center right;
+  }
+`;
+
+const Info = styled(motion.div)`
+  padding: 10px;
+  background-color: ${(props) => props.theme.black.lighter};
+  opacity: 0;
+  position: absolute;
+  width: 100%;
+  bottom: 0;
+  h4 {
+    text-align: center;
+    font-size: 18px;
+  }
+`;
+
+const rowVariants = {
+  hidden: {
+    x: window.outerWidth + 5,
+  },
+  visible: {
+    x: 0,
+  },
+  exit: {
+    x: -window.outerWidth - 5,
+  },
+};
+
+const boxVariants = {
+  normal: {
+    scale: 1,
+  },
+  hover: {
+    scale: 1.3,
+    y: -80,
+    transition: {
+      delay: 0.5,
+      duaration: 0.1,
+      type: "tween",
+    },
+  },
+};
+
+const infoVariants = {
+  hover: {
+    opacity: 1,
+    transition: {
+      delay: 0.5,
+      duaration: 0.1,
+      type: "tween",
+    },
+  },
+};
+
+const offset = 6;
+
+interface ISlider {
+  getData: () => Promise<IGetMoviesResult>;
+  id: string;
+}
+
+function Slider({ getData, id }: ISlider) {
+  const history = useHistory();
+  const [leaving, setLeaving] = useState(false);
+  const [index, setIndex] = useState(0);
+  const { data, isLoading } = useQuery<IGetMoviesResult>(
+    ["movies", id],
+    getData
+  );
+
+  console.log(id, data);
+
+  const toggleLeaving = () => setLeaving((prev) => !prev);
+  const onBoxClicked = (movieId: number) => {
+    history.push(`/movies/${movieId}`);
+  };
+
+  const incraseIndex = () => {
+    if (data) {
+      if (leaving) return;
+      toggleLeaving();
+      const totalMovies = data.results.length - 1;
+      const maxIndex = Math.floor(totalMovies / offset) - 1;
+      setIndex((prev) => (prev === maxIndex ? 0 : prev + 1));
+    }
+  };
+
+  return (
+    <SliderWrapper>
+      <AnimatePresence initial={false} onExitComplete={toggleLeaving}>
+        <Row
+          variants={rowVariants}
+          initial="hidden"
+          animate="visible"
+          exit="exit"
+          transition={{ type: "tween", duration: 1 }}
+          key={index}
+        >
+          {data?.results
+            .slice(1)
+            .slice(offset * index, offset * index + offset)
+            .map((movie) => (
+              <Box
+                key={movie.id}
+                bgPhoto={makeImagePath(movie?.backdrop_path, "w500")}
+                whileHover="hover"
+                initial="normal"
+                variants={boxVariants}
+                transition={{ type: "tween" }}
+                layoutId={movie.id + ""}
+                onClick={() => onBoxClicked(movie.id)}
+              >
+                <Info variants={infoVariants}>
+                  <h4>{movie.title}</h4>
+                </Info>
+              </Box>
+            ))}
+        </Row>
+      </AnimatePresence>
+    </SliderWrapper>
+  );
+}
+
+export default Slider;
